de86d28ca3f541714663ab27ddaf527ac5c3819d
Orbitz42 <95247743+Mert-T04@users.noreply.github.com>
2024-12-04 18:02:23 +0000

    day3

---
+++ Day3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllText("input.txt");
+var mulRegex = new Regex(@"mul\((\d{0,3}),(\d{0,3})\)|do\(\)|don't\(\)");
+var res = 0;
+var calcAllowed = true;
+
+foreach(Match match in mulRegex.Matches(input))
+{
+    if(match.Groups[0].Value == "do()") {
+        calcAllowed = true;
+        continue;
+    }
+    if(match.Groups[0].Value == "don't()") {
+        calcAllowed = false;
+        continue;
+    }
+
+    if(calcAllowed) {
+        res += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+    }
+}
+
+Console.WriteLine(res);
